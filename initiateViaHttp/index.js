1870d0ce98cb8efe84d8d522d12a386dd0d7633d
Adedapo Aderemi <adedapopaul@yahoo.com>
2023-05-01 13:41:27 +0100

    debugging http trgger

--- initiateViaHttp/index.js
+++ initiateViaHttp/index.js
@@ -8,10 +8,10 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-        extractBlogUrl();
-
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+
+    extractBlogUrl();
 }
