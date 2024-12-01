8a7b85ba0cdd403160f0e6863fa62f60a4b5386c
Adedapo Aderemi <adedapopaul@yahoo.com>
2024-12-01 17:01:14 +0000

    quick fix

--- db/general.js
+++ db/general.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-let isProd = ['production', 'local-production'].includes(process.env.NODE_ENV);
+let isProd = ["production", "local-production"].includes(process.env.NODE_ENV);
 
 exports.connection = {
-	database: process.env.LOGIN_DB_NAME, 
-	port: process.env.LOGIN_DB_PORT, 
-	host: process.env.LOGIN_DB_HOST,
-	user: process.env.LOGIN_DB_USERNAME,
-	password: process.env.LOGIN_DB_PASSWORD,
-	ssl: false
-}
+  database: process.env.LOGIN_DB_NAME,
+  port: process.env.LOGIN_DB_PORT,
+  host: process.env.LOGIN_DB_HOST,
+  user: process.env.LOGIN_DB_USERNAME,
+  password: process.env.LOGIN_DB_PASSWORD,
+  ssl: isProd,
+};
